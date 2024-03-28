6b59b620a4abf85161d06b2b43af0b9bb28f1d7a
malken21 <77374813+malken21@users.noreply.github.com>
2024-03-28 20:09:31 +0900

    WebUI起動時にチャットにメッセージが表示されるように変更しました、

--- src/main/java/marumasa/maruma_sign_webui/client/MarumaSignWebUIClient.java
+++ src/main/java/marumasa/maruma_sign_webui/client/MarumaSignWebUIClient.java
@@ -6,6 +6,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class MarumaSignWebUIClient implements ClientModInitializer {
@@ -14,13 +15,15 @@ public class MarumaSignWebUIClient implements ClientModInitializer {
 
         // キーバインド登録
         KeyBinding binding = KeyBindingHelper.registerKeyBinding(
-                Utils.createKeyBinding("open_menu", GLFW.GLFW_KEY_M)
+                Utils.createKeyBinding("open_menu", GLFW.GLFW_KEY_B)
         );
 
         // クライアントティックイベントにキーバインドの処理を登録
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) return;
             // もしキーが押されたら
             while (binding.wasPressed()) {
+                client.player.sendMessage(Text.translatable("chat.maruma_sign_webui.open_menu"), false);
                 ServerManager.openMenu();
             }
         });
